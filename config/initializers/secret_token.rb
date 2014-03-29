c2a5ea289bdf2fa32c7eaf12126b7634171c3aa7
Blake Erickson <o.blakeerickson@gmail.com>
2014-03-29 20:18:39 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lanyon::Application.config.secret_key_base = 'b9b9471bc3c1511b6610c97e292e3b4db9f35b54408ec43f6c00046855d86fb3f3d94393f53c5217c10b019884d0f6540759e34f2aa82d109cf8b71e2a798f02'
